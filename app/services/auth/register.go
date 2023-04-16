c74e3892a4b7f85f77776710806c4ace81647224
Johnson Huang <johnsonhuang724@gmail.com>
2023-04-16 06:45:12 +0000

    [fix] check duplicate email on register

--- app/services/auth/register.go
+++ app/services/auth/register.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	db "jd_workout_golang/lib/database"
+	"jd_workout_golang/app/models"
 )
 
 type registerForm struct {
@@ -22,26 +23,45 @@ func RegisterAction(c *gin.Context) {
 
 		return
 	}
+
 	hash, _ := bcrypt.GenerateFromPassword([]byte(registerForm.Password), bcrypt.DefaultCost)
 
-	user := user{
+	user := &models.User{
 		Username: registerForm.Username,
 		Email:    registerForm.Email,
 		Password: string(hash),
 	}
 
+	if state := validateRegister(user, db.InitDatabase()); !state {
+		c.JSON(422, gin.H{
+			"message": "Email 重複",
+			"error": "duplicate email",
+		})
+
+		return
+	}
+
 	db := db.InitDatabase()
 
-	db.AutoMigrate(&user)
-
-	storeUser(&user, db)
+	storeUser(user, db)
 
 	c.JSON(200, gin.H{
 		"message": "register success",
 	})
 }
 
-func storeUser(u *user, db *gorm.DB) *user {
+func validateRegister(u *models.User, db *gorm.DB) bool {
+	result := db.Where("email = ?", u.Email).First(&u)
+
+	// duplicate email
+	if result.RowsAffected == 1 {
+		return false
+	}
+
+	return true
+}
+
+func storeUser(u *models.User, db *gorm.DB) *models.User {
 	db.Create(u)
 
 	return u
