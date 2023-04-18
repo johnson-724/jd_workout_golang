a8dccbe92d43c9329b4a2b50e7c1b5c52791a629
tinghaolai <analth355@gmail.com>
2023-04-18 19:30:28 +0800

    Install swagger

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"jd_workout_golang/app/middleware"
+	docs "jd_workout_golang/docs"
 	"jd_workout_golang/internal/router"
 )
 
 func main() {
 	r := SetupRouter()
-
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.Run(":80") // listen and serve on
 }
 
