89faa9f64ba92af57a7ae0beaf6b88442f09aadb
Johnson Huang <johnsonhuang724@gmail.com>
2023-04-18 11:59:09 +0000

    [feat] add equip table

---
+++ database/migrations/2023_04_18_194954_createEquipTable.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"gorm.io/gorm"
+	"jd_workout_golang/lib/database"
+)
+
+type Equip struct {
+	gorm.Model
+	UserId  int    `gorm:"type:int;uniqueIndex:idx_user_equip"`
+	Name    string `gorm:"type:varchar(255);uniqueIndex:idx_user_equip"`
+	Weights string `gorm:"type:json"`
+	Note    string `gorm:"type:varchar(255)"`
+}
+
+func UpCreateEquipTable() {
+	db := database.InitDatabase()
+
+	db.Migrator().CreateTable(&Equip{})
+}
+
+func DownCreateEquipTable() {
+	db := database.InitDatabase()
+
+	db.Migrator().DropTable(&User{})
+}
