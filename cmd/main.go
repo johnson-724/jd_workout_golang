1fcf54fd1f7ba05275b62b8ba252ec55105b6c2a
tinghaolai <analth355@gmail.com>
2023-04-24 18:42:44 +0800

    sentry install

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
 	env "github.com/joho/godotenv"
 	swaggerfiles "github.com/swaggo/files"
@@ -8,6 +9,9 @@ import (
 	"jd_workout_golang/app/middleware"
 	docs "jd_workout_golang/docs"
 	"jd_workout_golang/internal/router"
+	"log"
+	"os"
+	"time"
 )
 
 // @securityDefinitions.apikey Bearer
@@ -17,6 +21,23 @@ import (
 // @scope.write Grants write access
 func main() {
 	env.Load()
+
+	sentryDsn := os.Getenv("SENTRY_DSN")
+	if sentryDsn != "" {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn:              sentryDsn,
+			TracesSampleRate: 1.0,
+		})
+
+		if err != nil {
+			log.Fatalf("sentry.Init: %s", err)
+		}
+
+		defer sentry.Flush(2 * time.Second)
+
+		sentry.CaptureMessage("手動發送 Test sentry error - type 3")
+	}
+
 	r := SetupRouter()
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
