ab26066067b51f5fbbb44336caf76942cda207e0
tinghaolai <analth355@gmail.com>
2023-05-27 16:05:24 +0800

    [Feat] Cors support patch

--- app/middleware/cors.go
+++ app/middleware/cors.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
