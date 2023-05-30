3e01c6f3c09ed16c818a719c4ffa50f6342635b3
Johnson Huang <johnsonhuang724@gmail.com>
2023-05-30 12:53:14 +0800

    [fix] revert accidentally annotations

--- lib/file/store.go
+++ lib/file/store.go
@@ -1,29 +1,29 @@
 package file
 
 import (
-	// "mime/multipart"
+	"mime/multipart"
 
-	// "github.com/gin-gonic/gin"
-	// f "jd_workout_golang/app/repositories/file"
+	"github.com/gin-gonic/gin"
+	f "jd_workout_golang/app/repositories/file"
 )
 
 
-// type FileStore interface{
-// 	Store(multipart.FileHeader, string) (string, error)
-// }
+type FileStore interface{
+	Store(multipart.FileHeader, string) (string, error)
+}
 
-// type GinFileStore struct {
-// 	File *multipart.FileHeader
-// 	Path string
-// }
+type GinFileStore struct {
+	File *multipart.FileHeader
+	Path string
+}
 
-// func Store(f f.FileStore) (string, error) {
+func Store(f f.FileStore) (string, error) {
 
-// 	f.Store()
+	f.Store()
 
-// 	gin := gin.Context{}
+	gin := gin.Context{}
 
-// 	gin.SaveUploadedFile(file, "./public/images/" + filepath)
+	gin.SaveUploadedFile(file, "./public/images/" + filepath)
 
-// 	return "", nil
-// }
+	return "", nil
+}
