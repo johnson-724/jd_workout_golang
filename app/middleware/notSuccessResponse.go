067a6d0e4c4e7acb84ca26037b232478bf736846
tinghaolai <analth355@gmail.com>
2023-06-11 08:05:58 +0800

    [Feat] Log full trace when panic

--- app/middleware/notSuccessResponse.go
+++ app/middleware/notSuccessResponse.go
@@ -5,6 +5,7 @@ import (
 	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
 	"log"
+	"runtime"
 	"time"
 )
 
@@ -14,6 +15,7 @@ func FailResponseAlert() gin.HandlerFunc {
 		defer func() {
 			if r := recover(); r != nil {
 				log.Printf("Recovered from panic: %v\n", r)
+				log.Printf("Stack trace:\n%s", getStackTrace())
 				sentry.CurrentHub().Recover(r)
 				sentry.Flush(time.Second * 5)
 
@@ -33,4 +35,10 @@ func FailResponseAlert() gin.HandlerFunc {
 			}
 		}
 	}
+}
+
+func getStackTrace() string {
+	buf := make([]byte, 1<<16)
+	stackSize := runtime.Stack(buf, true)
+	return string(buf[:stackSize])
 }
